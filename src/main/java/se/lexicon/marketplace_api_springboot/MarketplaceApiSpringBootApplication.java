c236aa03ec4739a310e00a11c59850e8e132bbd1
Saru-Jayaraman <sarumathijayaraman@gmail.com>
2024-10-17 14:24:23 +0530

    Init commit

---
+++ src/main/java/se/lexicon/marketplace_api_springboot/MarketplaceApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.marketplace_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/marketplace_api_springboot/MarketplaceApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.marketplace_api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
